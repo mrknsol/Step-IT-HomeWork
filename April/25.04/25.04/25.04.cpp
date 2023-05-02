bfbc2c315149436bd975a5f605dcd8d9921f7f94
Kenan Memmedli <mrkonsol@mail.ru>
2023-05-02 22:13:36 +0400

    files added

---
+++ April/25.04/25.04/25.04.cpp
@@ -0,0 +1,172 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Animal {
+    char* animalName = new char[30] {};
+    char* animalGender = new char[30];
+    int age{};
+
+
+    void printAnimalInfo() {
+        cout << "Animal Name: " << animalName << endl
+            << "Gender: " << animalGender << endl
+            << "Age: " << age << endl;
+    }
+};
+
+struct Zoo {
+    uint16_t zooCapacity{ 10 };
+    uint16_t countAnimals{};
+    char* zooName = new char[30];
+    Animal* animals{};
+
+
+    void createAnimal()
+    {
+        if (countAnimals < zooCapacity)
+        {
+            Animal* a = new Animal{};
+
+            getchar();
+
+            cout << "Enter animal name:";
+            cin.getline(a->animalName, 30);
+
+            cout << "Enter animal gender: ";
+            cin.getline(a->animalGender, 30);
+
+            cout << "Enter animal age: ";
+            cin >> a->age;
+
+            animals[countAnimals] = *a;
+            countAnimals++;
+        }
+        else {
+            cout << "Zoo is full!" << endl;
+        }
+    }
+
+    void editAnimal()
+    {
+        showAll();
+
+        int choice{};
+        cout << "Enter animal: " << endl;
+        cin >> choice;
+
+        getchar();
+
+        cout << "Enter new animal name: ";
+        cin.getline(animals[choice - 1].animalName, 30);
+
+        cout << "Enter new animal gender: ";
+        cin.getline(animals[choice - 1].animalGender, 30);
+
+        cout << "Enter new animal age: ";
+        cin >> animals[choice - 1].age;
+
+    }
+
+    void deleteAnimal()
+    {
+        showAll();
+
+        int choice{};
+        cout << "Choose animal to delete:" << endl;
+        cin >> choice;
+
+        getchar();
+
+        int index = choice - 1;
+        for (int i = choice - 1; i < zooCapacity; ++i)
+        {
+            if (animals[i].animalName == nullptr)
+                break;
+
+            index++;
+            animals[i].animalName = animals[i + 1].animalName;
+            animals[i].animalGender = animals[i + 1].animalGender;
+            animals[i].age = animals[i + 1].age;
+
+        }
+        animals[index].animalName = nullptr;
+        animals[index].animalGender = nullptr;
+        animals[index].age = 0;
+        countAnimals--;
+
+    }
+
+
+
+
+    void printInfo()
+    {
+        cout << "There are" << countAnimals << " animals in " << zooName << " ZOO" << endl;
+    }
+
+    void showAll()
+    {
+        for (int i = 0; i < countAnimals; i++) {
+            cout << "Animal #" << i + 1 << endl;
+            animals[i].printAnimalInfo();
+        }
+    }
+};
+
+void createZoo(Zoo*& zoo) {
+
+    zoo = new Zoo{};
+
+    cout << "Enter ZOO name: ";
+    cin.getline(zoo->zooName, 30);
+
+    cout << "Enter ZOO capacity: ";
+    cin >> zoo->zooCapacity;
+    getchar();
+
+    zoo->animals = new Animal[zoo->zooCapacity]{};
+
+}
+
+int main() {
+
+    int choice{};
+
+    Zoo* zoo{};
+    createZoo(zoo);
+
+
+    while (true)
+    {
+        cout << "Enter choice: " << endl
+            << "1. Add animal" << endl
+            << "2. Delete animal" << endl
+            << "3. Edit animal" << endl
+            << "4. Show animals" << endl;
+
+        cin >> choice;
+
+
+        switch (choice) {
+        case 1:
+            system("cls");
+            zoo->createAnimal();
+            break;
+        case 2:
+            system("cls");
+            zoo->deleteAnimal();
+            break;
+        case 3:
+            system("cls");
+            zoo->editAnimal();
+            break;
+        case 4:
+            system("cls");
+            zoo->showAll();
+            break;
+        default:
+            break;
+        }
+    }
+    return 0;
+}
---
+++ April/30.04/30.04/30.04.cpp
@@ -0,0 +1,177 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Student
+{
+	char* name = new char[31] {};
+	char* surname = new char[31] {};
+	int age{};
+	int faculty{};
+	int average{};
+	int course{};
+};
+
+struct Academy
+{
+	char* name = new char[31] {};
+	int c[4]{};
+	Student* IT = new Student[30]{};
+	Student* Basketball = new Student[30]{};
+	Student* Football = new Student[30]{};
+	Student* Baseball = new Student[30]{};
+
+	void addStudent()
+	{
+		Student* s = new Student{};
+		getchar();
+
+		cout << "Enter name: "; cin.getline(s->name, 30);
+		cout << "Enter surname: "; cin.getline(s->surname, 30);
+		cout << "Enter age: "; cin >> s->age;
+		cout << "Enter average: "; cin >> s->average;
+		cout << "Enter course: "; cin >> s->course;
+
+		cout
+			<< "Enter faculty" << endl
+			<< "1. IT" << endl
+			<< "2. Basketball" << endl
+			<< "3. Football" << endl
+			<< "4. Baseball" << endl;
+		cin >> s->faculty;
+		switch (s->faculty)
+		{
+		case 1:
+			IT[c[0]] = *s;
+			c[0]++;
+			break;
+		case 2:
+			Basketball[c[1]] = *s;
+			c[1]++;
+			break;
+		case 3:
+			Football[c[2]] = *s;
+			c[2]++;
+			break;
+		case 4:
+			Baseball[c[3]] = *s;
+			c[3]++;
+		default:
+			break;
+		}
+	}
+
+	int printStud()
+	{
+		int choice{};
+		cout
+			<< "Choice Faculty: " << endl
+			<< "1. IT" << endl
+			<< "2. Basketball" << endl
+			<< "3. Football" << endl
+			<< "4. Baseball" << endl;
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 1:
+			for (size_t i = 0; i < c[0]; i++)
+			{
+				cout << i + 1 << ' ' << IT[i].name << endl;
+			}
+			break;
+		case 2:
+			for (size_t i = 0; i < c[1]; i++)
+			{
+				cout << i + 1 << ' ' << Basketball[i].name << endl;
+			}
+			break;
+		case 3:
+			for (size_t i = 0; i < c[2]; i++)
+			{
+				cout << i + 1 << ' ' << Football[i].name << endl;
+			}
+			break;
+		case 4:
+			for (size_t i = 0; i < c[3]; i++)
+			{
+				cout << i + 1 << ' ' << Baseball[i].name << endl;
+			}
+			break;
+		default:
+			break;
+		}
+
+		return choice;
+	}
+
+	void deleteStudent(int facult)
+	{
+		int choice{};
+		cout << "Choose student to delete:" << endl;
+		cin >> choice;
+
+		getchar();
+
+		switch (facult)
+		{
+		case 1:
+			for (int i = choice - 1; i < c[0] - 1; ++i)
+			{
+				IT[i].name = IT[i + 1].name;
+				IT[i].surname = IT[i + 1].surname;
+				IT[i].faculty = IT[i + 1].faculty;
+				IT[i].age = IT[i + 1].age;
+				IT[i].average = IT[i + 1].average;
+				IT[i].course = IT[i + 1].course;
+			}
+			c[0]--;
+			break;
+		default:
+			break;
+		}
+
+	}
+};
+
+void createAcademy(Academy*& academy)
+{
+	cout << "Enter name: "; cin.getline(academy->name, 30);
+}
+
+int main()
+{
+	Academy* academy = new Academy{};
+	createAcademy(academy);
+
+	while (true)
+	{
+		int choice{};
+
+		cout
+			<< "1. Add Student" << endl
+			<< "2. Delete Student" << endl
+			<< "3. Show Faculty student" << endl;
+		cin >> choice;
+
+
+		switch (choice)
+		{
+		case 1:
+			system("cls");
+			academy->addStudent();
+			break;
+		case 2:
+			system("cls");
+			academy->deleteStudent(academy->printStud());
+			break;
+		case 3:
+			system("cls");
+			academy->printStud();
+			break;
+		default:
+			break;
+		}
+	}
+
+	return 0;
+}
