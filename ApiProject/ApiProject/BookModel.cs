e748df650fe66bc8f5119c8367bc1f4ca7d76c3b
mrknsol <kenanmemmedli055@gmail.com>
2024-03-31 11:17:30 +0400

    files added

---
+++ ApiProject/ApiProject/BookModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ApiProject
+{
+    public class BookModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ ApiProject/ApiProject/OpenLibraryAPI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ApiProject
+{
+    public class OpenLibraryAPI : IOpenLibraryAPI
+    {
+        private readonly HttpClient _httpClient;
+
+        public OpenLibraryAPI(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<BookModel> GetBookInfo(string bookId)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"https://openlibrary.org/api/books/{bookId}.json");
+            response.EnsureSuccessStatusCode();
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+            dynamic data = JsonConvert.DeserializeObject(jsonResponse);
+
+            var bookModel = new BookModel
+            {
+                Title = data.title,
+                Author = data.authors[0].name,
+                Genre = "", 
+                Year = data.publish_date != null ? Convert.ToInt32(data.publish_date.Split(' ')[2]) : 0
+            };
+
+            return bookModel;
+        }
+    }
+}
